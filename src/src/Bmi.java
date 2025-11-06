08f9dfbaae2ca79d22e1f0f81ffab5c528fe87f3
svanholm200-debug <svanholm200@gmail.com>
2025-11-06 15:03:48 +0100

    Fixed bmi calculatoren så den regner rigtigt

--- src/src/Bmi.java
+++ src/src/Bmi.java
@@ -1,6 +1,6 @@
 public class Bmi {
     public static double getBmi(double weight, double height) {
-        double bmi = weight / (height * height);
+        double bmi = weight / (Math.pow(height, 2));
         return bmi;
     }
 }
--- src/src/Main.java
+++ src/src/Main.java
@@ -2,15 +2,14 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        System.out.println("Hvor høj er du");
         Scanner sc = new Scanner(System.in);
+        System.out.println("Hello, World!");
+        System.out.println("Hvor meget vejer du i kg?");
+        double weight = sc.nextDouble();
+        System.out.println("Hvor høj er du i meter?");
         double height = sc.nextDouble();
-        System.out.println("Hvor meget vejer du?");
-        Scanner sc2 = new Scanner(System.in);
-        double weight = sc2.nextDouble();
-        System.out.println("Du er " + height + "cm " + "Og du vejer " + weight + "kg" + " og dit bmi er " + Bmi.getBmi(height,weight));
-        if(Bmi.getBmi(weight, height) <= 20){
+        System.out.println("Du er " + height + "cm " + "Og du vejer " + weight + "kg" + " og dit bmi er " + Bmi.getBmi(weight,height));
+        if(Bmi.getBmi(weight, height) >= 20){
             System.out.println("Du er sej");
         }else{ System.out.println("Du er ikke sej");
             }
