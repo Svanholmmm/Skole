5ece454dc9dbd48c1c69acc8d6c1ab2a94ffcdd2
svanholm200-debug <svanholm200@gmail.com>
2025-11-06 14:18:05 +0100

    +Bmi calculator

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        double height = 190.0;
+        double weight = 90.0;
+        double bmi = weight / (height * height);
+        System.out.println(bmi);
     }
 }
